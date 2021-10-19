1ef40ca985069ad684365a03ccb6b56f9c3d2889
Putu Angga Suta Dharmawan <angga.suta92@gmail.com>
2021-10-19 09:40:54 +0800

    update

--- src/java/com/peramalan/config/MainConfig.java
+++ src/java/com/peramalan/config/MainConfig.java
@@ -19,7 +19,7 @@ public class MainConfig {
     * Database cofiguration
     */
     public static final String DATABASE_DRIVER= "com.mysql.jdbc.Driver";
-    public static final String DATABASE_URL= "jdbc:mysql://localhost:3306/skripsi_apps";
+    public static final String DATABASE_URL= "jdbc:mysql://localhost:3306/inconvet";
     public static final String DATABASE_USER= "root";
     public static final String DATABASE_PASSSWORD= "root";
     
